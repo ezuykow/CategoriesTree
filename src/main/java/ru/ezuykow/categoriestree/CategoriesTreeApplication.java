3b42c77b4706b6895d8f43aa79bb9c8dfa14ce44
ezuykow <zuy1996@gmail.com>
2023-09-25 09:08:26 +0300

    Initial commit

---
+++ src/main/java/ru/ezuykow/categoriestree/CategoriesTreeApplication.java
@@ -0,0 +1,13 @@
+package ru.ezuykow.categoriestree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoriesTreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CategoriesTreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ezuykow/categoriestree/CategoriesTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ezuykow.categoriestree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoriesTreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
